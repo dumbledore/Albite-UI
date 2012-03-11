0f25384dca93cfeef0fd6ea6c35ef406c7399058
Svetlin Ankov <galileostudios@gmail.com>
2012-03-11 23:06:31 +0200

    Started working on the SectionControl

---
+++ src/org/albite/ui/controls/SectionControl.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.albite.ui.controls;
+
+import javax.microedition.lcdui.Graphics;
+import org.albite.ui.core.interfaces.Context;
+
+/**
+ *
+ * @author albus
+ */
+public class SectionControl extends Control {
+
+    public SectionControl(final Control parent, final Context context) {
+        super(parent, context);
+    }
+
+    protected void draw(Graphics g, int x, int y) {
+    }
+}
